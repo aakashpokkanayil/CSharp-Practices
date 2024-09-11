e2a5b77156ea02d20215834e67a91fa501d7d209
Aakash P <aakashpokkanayil@gmail.com>
2024-09-11 19:42:40 +0530

    Add project files.

---
+++ testApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("What is your name?");
+string? name = Console.ReadLine();
+Console.WriteLine($"Hi {name}");
+Console.WriteLine("What is your age?");
+string? age = Console.ReadLine();
+Console.WriteLine($"Age entered is {age}");
